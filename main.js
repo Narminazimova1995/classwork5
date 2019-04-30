006ac0725e182bceaa270bcd4acf929b6584c741
Narminazimova1995 <narminazade@mail.ru>
2019-04-30 17:42:48 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+let fabars = document.querySelector(".icons");
+
+fabars.onclick=function(){
+   
+  let togg= document.getElementsByClassName("menu")[0].classList.toggle("active")
+   let fabars =document.querySelector(".fa-bars")
+  let close = fabars.classList.toggle("fa-times");
+      
+}
+
